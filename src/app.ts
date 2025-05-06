5292ed934efe8da48b3a3898556209451a7f81db
Tahsin <gazitahsin323@gmail.com>
2025-05-06 10:10:36 +0600

    `Update dependencies and add error handling middleware`

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import userRoutes from './routes/user.routes'
 import errorMiddleware from './middlewares/error.middleware'
+import { globalErrorHandler } from './middlewares/globalErrorHandler'
+import { notFound } from './middlewares/notFound'
 
 const app = express()
 
@@ -9,5 +11,9 @@ app.use(express.json())
 app.use('/api/users', userRoutes)
 
 app.use(errorMiddleware)
+app.use(globalErrorHandler);
+
+/** ------------ NOT FOUND URL ------------------- */
+app.use(notFound as never);
 
 export default app
