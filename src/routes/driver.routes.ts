c9621476ea8d4bcfc86b0dee2b076752558cfdd3
Tahsin <gazitahsin323@gmail.com>
2025-05-15 15:47:20 +0600

    `Added driverScheduledTrips endpoint and updated routes, controllers and middleware to include it`

--- src/routes/driver.routes.ts
+++ src/routes/driver.routes.ts
@@ -5,12 +5,15 @@ import {
   getAllDrivers,
   updateDriver,
   deleteDriver,
+  driverScheduledTrips,
 } from '../controllers/driver.controller'
+import { isDriver, protect } from '../middlewares/auth.middleware'
 const router = express.Router()
 
 router.post('/add/driver',upload.single("avatar"),  addDriver)
 router.get('/all/drivers', getAllDrivers)
 router.put('/drivers', updateDriver)
 router.delete('/drivers/:id', deleteDriver)
+router.get('/drivers/schedule',protect,isDriver, driverScheduledTrips)
 
 export default router
