2a965a51e5de4389c61f4ff3a0dd30dba192ee45
Tahsin <gazitahsin323@gmail.com>
2025-05-24 11:03:27 +0600

    `Added new API endpoints for getting all bus routes and updated user controller to include new endpoint`

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,21 +4,22 @@ import AppError from '../errors/AppError'
 import { User } from '../models/user.model'
 import httpStatus from "http-status";
 import { Ticket } from '../models/ticket.model';
+import { Bus } from '../models/bus.model';
 
 
 
-export const updateProfile = catchAsync(async (req, res) =>{
-  const {name, email, password, phone, username } = req.body;
-  const user = await User.findByIdAndUpdate({_id: req?.user?._id},{name,phone},{new:true});
-  if(!user){
-      throw new AppError(httpStatus.NOT_FOUND, 'User not found')
-      }
-    sendResponse(res,{
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Profile updated',
-      data: user
-    })
+export const updateProfile = catchAsync(async (req, res) => {
+  const { name, email, password, phone, username } = req.body;
+  const user = await User.findByIdAndUpdate({ _id: req?.user?._id }, { name, phone }, { new: true });
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found')
+  }
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Profile updated',
+    data: user
+  })
 
 })
 
@@ -40,15 +41,34 @@ export const getUsers = catchAsync(async (req, res) => {
 
 
 
-export const allRide = catchAsync(async (req, res) =>{
-  if(!req.user?._id){
+export const allRide = catchAsync(async (req, res) => {
+  if (!req.user?._id) {
     throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized to access this route')
   }
-  const ride = await Ticket.find({userId: req.user?._id}).select("-avaiableSeat")
-  sendResponse(res,{
+  const ride = await Ticket.find({ userId: req.user?._id }).select("-avaiableSeat")
+  sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Get all ride',
     data: ride
+  })
+})
+
+
+
+export const allBusRoute = catchAsync(async (req, res) => {
+  const busRoute = await Bus.find().select("stops")
+  let Route:string[] = []
+  busRoute.forEach((item) => {
+    item.stops.forEach((item2) => {
+      Route.push(item2.name)
+    })
+  }
+  )
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get all bus route',
+    data: Route
     })
 })
