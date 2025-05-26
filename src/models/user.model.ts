31e5acff0b73d1dafe84631f950376c6c40d8441
Tahsin <gazitahsin323@gmail.com>
2025-05-26 10:03:39 +0600

    `feat: updated driver controller, user interface, and model with new features and fields`

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -9,6 +9,7 @@ const userSchema: Schema = new Schema<IUser>(
     email: { type: String, required: true, unique: true },
     password: { type: String, select: 0, required: true },
     username: { type: String, required: true, unique: true },
+    phone : { type: String },
     credit: { type: Number, default: null },
     role: {
       type: String,
